7add1d74845c3d2d71f34d94f3bca38479bcf746
MH-TECHSHOW <103843668+MH-TECHSHOW@users.noreply.github.com>
2023-01-06 08:52:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
 global.owner = ['94711620069'] //ur owner number
-global.ownername = "Kaveesha Sithum" //ur owner name
+global.ownername = "Lithum Helitha" //ur owner name
 global.ytname = "YT: kavee" //ur yt chanel name
 global.socialm = "Dark Nero" //ur github or insta name
 global.location = "Sri Lanka, Gampaha, Kurunegala" //ur location
